6b9854bb522fc475b4b075b6851dc336a7c25d21
Simone Stumpf <Simone.Stumpf@glasgow.com>
2022-01-31 18:53:27 +0000

    chapter 5

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("rango says hey there partner!")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context = context_dict)
+
+def about(request):
+    context_dict = {{ MEDIA_URL}}
+    return render(request, 'rango/about.html', context = context_dict)
